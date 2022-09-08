a6c5b00ade8bd37717b8f6baf5a2c2598acd16f5
m0n7h0ff <al.monah.off@gmail.com>
2022-09-08 15:38:07 +0500

    tmp

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	date_calc "github.com/m0n7h0ff/date-calc"
+	"github.com/m0n7h0ff/date-calc/pkg/repository"
 	"log"
 	"math"
 	"os"
@@ -12,15 +13,18 @@ import (
 )
 
 func main() {
-	mapOfEmployee := setEmployeeList()
-	ReadAndPrint(mapOfEmployee)
-	//что-бы консоль не закрывалась сразу
+	mapOfEmployee := repository.SetEmployeeList()
+	answer := getAnswer(mapOfEmployee)
+	fmt.Println(answer)
+	DoNotCloseTheConsole()
+}
+
+func DoNotCloseTheConsole() {
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	fmt.Println(input)
 }
-
-func ReadAndPrint(mapOfEmployee map[string]date_calc.Employee) {
+func getAnswer(mapOfEmployee map[string]date_calc.Employee) string {
 	enteredName := readInput("Введите фамилию:\t")
 
 	foundEmployee, ok := mapOfEmployee[enteredName]
@@ -33,44 +37,18 @@ func ReadAndPrint(mapOfEmployee map[string]date_calc.Employee) {
 	parseDate, err := time.Parse("2006-01-02", enteredDate)
 	if err != nil {
 		log.Printf("Неправильный формат даты: %v \n", err)
-		reader := bufio.NewReader(os.Stdin)
-		input, _ := reader.ReadString('\n')
-		fmt.Println(input)
+		DoNotCloseTheConsole()
 	}
 
 	differenceBetweenDates := getDifferenceBetweenDates(startDate, parseDate)
 	_, fractional := math.Modf(float64(differenceBetweenDates) / 8.0) //остаток
 	fmt.Println()
-	printResult(fractional)
+	return getScheduleAnswer(fractional)
 }
-
-func setEmployeeList() map[string]date_calc.Employee {
-	emp1 := new(date_calc.Employee)
-	emp1.NewEmployee("Монахов", 2022, 6, 3)
-
-	emp2 := new(date_calc.Employee)
-	emp2.NewEmployee("Дубинин", 2022, 6, 5)
-
-	emp3 := new(date_calc.Employee)
-	emp3.NewEmployee("Тюшняков", 2022, 6, 1)
-
-	emp4 := new(date_calc.Employee)
-	emp4.NewEmployee("Перехода", 2022, 6, 7)
-
-	mapOfEmployee := make(map[string]date_calc.Employee)
-	mapOfEmployee[emp1.FIO] = *emp1
-	mapOfEmployee[emp2.FIO] = *emp2
-	mapOfEmployee[emp3.FIO] = *emp3
-	mapOfEmployee[emp4.FIO] = *emp4
-
-	return mapOfEmployee
-}
-
 func getDifferenceBetweenDates(startDate time.Time, enteredDate time.Time) int {
 	days := enteredDate.Sub(startDate).Hours() / 24
 	return int(days)
 }
-
 func readInput(message string) string {
 	fmt.Print(message)
 	reader := bufio.NewReader(os.Stdin)
@@ -83,23 +61,21 @@ func readInput(message string) string {
 	}
 	return strings.TrimSpace(input)
 }
-
-//в веб
-func printResult(ost float64) {
+func getScheduleAnswer(ost float64) string {
 	switch ost {
 	case 0:
-		fmt.Println("1-я смена, в день")
+		return fmt.Sprint("1-я смена, в день")
 	case 0.125:
-		fmt.Println("2-я смена, в день")
+		return fmt.Sprint("2-я смена, в день")
 	case 0.75, 0.25:
-		fmt.Println("1й выходной")
+		return fmt.Sprint("1й выходной")
 	case 0.375, 0.875:
-		fmt.Println("2й выходной")
+		return fmt.Sprint("2й выходной")
 	case 0.5:
-		fmt.Println("1-я смена, в ночь")
+		return fmt.Sprint("1-я смена, в ночь")
 	case 0.625:
-		fmt.Println("2-я смена, в ночь")
+		return fmt.Sprint("2-я смена, в ночь")
 	default:
-		fmt.Println("Fail/////")
+		return fmt.Sprint("Дата неверна")
 	}
 }
---
+++ pkg/repository/repository.go
@@ -0,0 +1,25 @@
+package repository
+
+import date_calc "github.com/m0n7h0ff/date-calc"
+
+func SetEmployeeList() map[string]date_calc.Employee {
+	emp1 := new(date_calc.Employee)
+	emp1.NewEmployee("Монахов", 2022, 6, 3)
+
+	emp2 := new(date_calc.Employee)
+	emp2.NewEmployee("Дубинин", 2022, 6, 5)
+
+	emp3 := new(date_calc.Employee)
+	emp3.NewEmployee("Тюшняков", 2022, 6, 1)
+
+	emp4 := new(date_calc.Employee)
+	emp4.NewEmployee("Перехода", 2022, 6, 7)
+
+	mapOfEmployee := make(map[string]date_calc.Employee)
+	mapOfEmployee[emp1.FIO] = *emp1
+	mapOfEmployee[emp2.FIO] = *emp2
+	mapOfEmployee[emp3.FIO] = *emp3
+	mapOfEmployee[emp4.FIO] = *emp4
+
+	return mapOfEmployee
+}
