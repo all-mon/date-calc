63fd2f652ab22d96a8974de38c6b7424582c62fd
m0n7h0ff <al.monah.off@gmail.com>
2022-10-21 23:03:33 +0500

    Добавил форму с автозаполнением на стр с расписанием. Настроил вывод графика каждого дежурного

--- web/js/schdl.js
+++ web/js/schdl.js
@@ -1,16 +1,17 @@
 function loadDoc() {
+    var x = document.getElementById("myInput").value;
+    console.log(x);
     var xhttp = new XMLHttpRequest();
+
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var resp = JSON.parse(xhttp.response);
-            console.log(JSON.parse(xhttp.response))
+            //console.log(JSON.parse(xhttp.response))
+            //console.log(resp[0].sch)
 
-            console.log(resp[0].sch)
             var table = document.getElementById("test");
             for (var i = 0; i < 5; i++) {
                 var tr = document.createElement('tr')
-
-
                 for (var j = 0; j < 2; j++) {
                     var td = document.createElement('td')
                     if (j === 0) td.innerHTML = resp[i].date;
@@ -19,11 +20,9 @@ function loadDoc() {
                 }
                 table.appendChild(tr)
             }
-
-
         }
     };
-    xhttp.open("GET", "http://127.0.0.1:8080/api/%D0%9C%D0%BE%D0%BD%D0%B0%D1%85%D0%BE%D0%B2", true);
+    xhttp.open("GET", "http://127.0.0.1:8080/api/"+x, true);
     xhttp.send();
 }
 
