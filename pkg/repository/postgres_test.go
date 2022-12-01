e2a7b03c80b21714cbf0adc0e4e08c90c28c3f1c
m0n7h0ff <al.monah.off@gmail.com>
2022-12-01 23:58:54 +0500

    add test

--- pkg/repository/postgres_test.go
+++ pkg/repository/postgres_test.go
@@ -1,23 +1,49 @@
 package repository
 
 import (
-	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-	"os"
 	"testing"
 )
 
-func Test_connect(t *testing.T) {
-	_, err := NewPostgresDB(Config{
+func initConfig() error {
+	viper.AddConfigPath("../../config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
+func RunPostgresDB() (*sqlx.DB, error) {
+	db, err := NewPostgresDB(Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
 		DBName:   viper.GetString("db.dbname"),
 		SSLMode:  viper.GetString("db.sslmode"),
-		Password: os.Getenv("DB_PASSWORD"),
+		Password: "123123",
 	})
+	return db, err
+}
+
+func Test_connect(t *testing.T) {
+
+	if err := initConfig(); err != nil {
+		t.Errorf("error initConfig: %s", err.Error())
+	}
+
+	_, err := RunPostgresDB()
 	if err != nil {
-		fmt.Println(err)
-		return
+		t.Errorf("error connecting to database: %s", err.Error())
+	}
+}
+func TestEmployeePostgres_GetByLastName(t *testing.T) {
+	db, _ := RunPostgresDB()
+	employeePostgres := EmployeePostgres{db: db}
+	employee, err := employeePostgres.GetByLastName("Монахов")
+	if err != nil {
+		t.Error(err)
+	}
+	if employee.Lastname != "Монахов" {
+		t.Error(err)
 	}
 }
